dd90f96e8be6ab9f28892e9682391eca57d62070
JohnNils Olson <johnnils@gmail.com>
2020-08-05 11:19:02 -0700

    add database migrations
    Co-authored-by: Ben Russell <benjaminrussell36@gmail.com>

---
+++ TravelTracker/Migrations/20200805181619_initial.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TravelTracker.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Destinations",
+                columns: table => new
+                {
+                    DestinationId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    CityName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Destinations", x => x.DestinationId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Travellers",
+                columns: table => new
+                {
+                    TravellerId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Travellers", x => x.TravellerId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DestinationTraveller",
+                columns: table => new
+                {
+                    DestinationTravellerId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    DestinationId = table.Column<int>(nullable: false),
+                    TravellerId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DestinationTraveller", x => x.DestinationTravellerId);
+                    table.ForeignKey(
+                        name: "FK_DestinationTraveller_Destinations_DestinationId",
+                        column: x => x.DestinationId,
+                        principalTable: "Destinations",
+                        principalColumn: "DestinationId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DestinationTraveller_Travellers_TravellerId",
+                        column: x => x.TravellerId,
+                        principalTable: "Travellers",
+                        principalColumn: "TravellerId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DestinationTraveller_DestinationId",
+                table: "DestinationTraveller",
+                column: "DestinationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DestinationTraveller_TravellerId",
+                table: "DestinationTraveller",
+                column: "TravellerId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DestinationTraveller");
+
+            migrationBuilder.DropTable(
+                name: "Destinations");
+
+            migrationBuilder.DropTable(
+                name: "Travellers");
+        }
+    }
+}
