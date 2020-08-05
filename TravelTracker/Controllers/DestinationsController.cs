769835ad533721f366a0c0d7ad7bb72539b8c470
JohnNils Olson <johnnils@gmail.com>
2020-08-05 11:58:19 -0700

    add details and edit routes
    Co-authored-by: Ben Russell <benjaminrussell36@gmail.com>

--- TravelTracker/Controllers/DestinationsController.cs
+++ TravelTracker/Controllers/DestinationsController.cs
@@ -9,10 +9,12 @@ namespace TravelTracker.Controllers
   public class DestinationsController : Controller
   {
     private readonly TravelTrackerContext _db;
+
     public DestinationsController(TravelTrackerContext db)
     {
       _db = db;
     }
+
     public ActionResult Index()
     {
       List<Destination> model = _db.Destinations.ToList();
@@ -28,6 +30,29 @@ namespace TravelTracker.Controllers
     public ActionResult Create(Destination destination)
     {
       _db.Destinations.Add(destination);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      var thisDestination = _db.Destinations
+        .Include(destination => destination.Travellers)
+        .ThenInclude(join => join.Traveller)
+        .FirstOrDefault(destination => destination.DestinationId == id);
+      return View(thisDestination);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      var thisDestination = _db.Destinations.FirstOrDefault(destinations => destinations.DestinationId == id);
+      return View(thisDestination);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Destination destination)
+    {
+      _db.Entry(destination).State = EntityState.Modified;
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
